d03f3132aa2932cb295a1612b6723eb6fdcf2df6
PatricioSN <patriciosppb@gmail.com>
2026-05-04 17:27:57 -0300

    add comentarios: charset.py

--- charset.py
+++ charset.py
@@ -1,5 +1,6 @@
-import itertools
+# GABS este arquivo serve para gerar as senhas
 
+import itertools
 # A ordem importa: letras → números → especiais
 
 LETRAS_MINUSCULAS = "abcdefghijklmnopqrstuvwxyz"
@@ -7,6 +8,8 @@ LETRAS_MAIUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 NUMEROS = "0123456789"
 ESPECIAIS = "!@#$%&*()_-+="
 
+# Montar_charset é definido pelo usuário qual o tipo de senha ele quer tentar
+# ex: o User pode querer usar apenas letra na senha minusculas, então ele usa -l
 def montar_charset(letras_min: bool, letras_mai: bool, numeros: bool, especiais: bool) -> str:
     charset = ""
     if not any ([letras_min, letras_mai, numeros, especiais]):
@@ -23,7 +26,7 @@ def montar_charset(letras_min: bool, letras_mai: bool, numeros: bool, especiais:
 
     return charset
 
-
+# Aqui, ele pega a string formada pelo "montar_charset", essa string se chama charset
 def gerar_senhas(tamanho_max: int, letras_min: bool = False, letras_mai: bool = False,
                  numeros: bool = False, especiais: bool = False):
     charset = montar_charset(letras_min, letras_mai, numeros, especiais)
